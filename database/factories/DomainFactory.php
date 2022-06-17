5c912f08af451943d7123462168bcf60a1cb0574
MIKO <qobilov.mirobid@gmail.com>
2022-06-17 14:14:10 +0500

    domain_factory

---
+++ database/factories/DomainFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Domain>
+ */
+class DomainFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->domainName,
+            'ping' => $this->faker->numberBetween(50, 1200)
+
+        ];
+    }
+}
